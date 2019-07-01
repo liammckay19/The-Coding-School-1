e5634bedaeb3256801b42840e297019d4a22e78f
Liam McKay <ltmckay@ucsc.edu>
2019-07-01 09:51:35 -0700

    mutable examlple

---
+++ Liam/mutable.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+dictA = { "key": "value" }
+dictA["key"] = "newValue"
+
+dictA = { "key": "newValue" }
+
+
+
+a = { 'b': 4 }
+z = a  # z = {'b':4}
+a['b'] = 5 # a = {'b':5}
+z # {'b':5}
+
+# mutable!
---
+++ Liam/u1l3variables.py
@@ -0,0 +1,2 @@
+x = 10
+# the variable named x (in our computers memory) was declared to have the value of 10, an integer type
