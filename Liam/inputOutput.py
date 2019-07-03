16c7e0bbf87ca215accc5dfaa45104cc44ed8deb
Liam McKay <ltmckay@ucsc.edu>
2019-07-03 14:08:27 -0700

    rpg project player example

---
+++ Liam/inputOutput.py
@@ -0,0 +1,15 @@
+import io
+
+# userInput = input("What is your favorite number?")
+
+# print(userInput)
+
+# define number x
+# multiply x by 3
+
+x = input("x = ")
+print(int(x)*3)  # casting the string x into an integer object/variable type
+
+y = input("y = ")
+result = int(y)*3
+
