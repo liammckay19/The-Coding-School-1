d51b53f397d1d021fd05bca70f05266cd491167f
Liam McKay <ltmckay@ucsc.edu>
2019-07-10 10:49:40 -0700

    improving (instructor)

--- holden/input and output.py
+++ holden/input and output.py
@@ -1,9 +1,12 @@
-s = input('what is your name?')
-a = input('what is your last name?')
-d = input('what year were you born?')
-print('**********hello', s, a, '|*********')
-print('you were born in 0000' + str(d))
-print('the current year is 2.019 × 10^3.')
-print('you are ',2019 - int(d),' years old')
-print('your mystical magic percentage is',int(d)/2019*100)
-print('have a good day and farewell')
+userFirst = input('what is your name?')
+userLast = input('what is your last name?')
+userBirthyear = input('what year were you born?')
+currentYear = 2019
+
+greeting = 'Hello ' + userFirst + ' ' + userLast + '!'
+print(greeting.center(50, '*'))
+print('you were born in 0000' + str(userBirthyear))
+print('the current year is {0:0.3e}'.format(currentYear))
+print('you are', 2019 - int(userBirthyear), 'years old')
+print('your mystical magic percentage is {0:0.4f}'.format(int(userBirthyear) / 2019 * 100))
+print('Goodbye!'.rjust(50,' '))
